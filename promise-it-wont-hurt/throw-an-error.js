244b353ad736bfdf22cf597ccbbacdc098914f48
Paolo Laurenti <laurentipaolo@gmail.com>
2015-08-15 17:14:24 +0200

    Complete 'Throw an error' challenge in 'Promise it won't hurt' tutorial

---
+++ promise-it-wont-hurt/throw-an-error.js
@@ -0,0 +1,17 @@
+var q = require('q');
+
+var jsonArg = process.argv[2];
+
+var parsePromised = function(json) {
+  var defer = q.defer();
+  try {
+    var a = JSON.parse(json);
+    defer.resolve(a);
+  } catch (e) {
+    defer.reject(e);
+  }
+  return defer.promise;
+};
+
+parsePromised(jsonArg)
+  .then(null, console.log);
