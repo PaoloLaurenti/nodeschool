485640c75ca51e51d5376970fec9f9ad1c9a854e
Paolo Laurenti <laurentipaolo@gmail.com>
2015-08-16 11:20:44 +0200

    Complete 'Using qfcall' challenge in 'Promise it won't hurt' tutorial

---
+++ promise-it-wont-hurt/using_qfcall.js
@@ -0,0 +1,18 @@
+var Q = require('q');
+
+var jsonArg = process.argv[2];
+
+var parsePromised = function(json) {
+  var defer = q.defer();
+  try {
+    var a = JSON.parse(json);
+    defer.resolve(a);
+  } catch (e) {
+    defer.reject(e);
+  }
+  return defer.promise;
+};
+
+//parsePromised(jsonArg)
+Q.fcall(JSON.parse, jsonArg)
+  .then(null, console.log);
